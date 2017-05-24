085a6a0a030f4190470eed950175274dd82e7cbf
Manuel Schmalstieg <git.code@ms-studio.net>
2017-05-24 18:39:49 +0200

    Correctif JS
    Le fichier svg-animate.js comportait des résidus de “git merge” (fusion de fichiers avec Git), ce qui causait des erreurs JavaScript.

--- js/svg-animate.js
+++ js/svg-animate.js
@@ -1,30 +1,21 @@
-<<<<<<< HEAD
 // JavaScript Document
-=======
->>>>>>> origin/master
+
 /*
  * SVG Animate
  */
 
 // Le code suivant charge le fichier SVG externe, 
 // afin de le rendre accessible par Javascript
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
 var svgExt = document.getElementById("carteeurope");
 var svgDoc;
 
-console.log(svgExt);
-
 // On définit tous les objets SVG qu'on souhaite ensuite manipuler
 var svgEspagne;
-<<<<<<< HEAD
 var svgUk;
 var svgAllemagne;
 var svgLuxembourg;
-=======
->>>>>>> origin/master
+
 
 svgExt.addEventListener("load",function() {
 
@@ -34,7 +25,6 @@ svgExt.addEventListener("load",function() {
   // Le SVG est chargé, on peut maintenant
   // définir les formes qu'on souhaite manipuler
   svgEspagne = svgDoc.getElementById("espagne");
-<<<<<<< HEAD
   svgUk = svgDoc.getElementById("uk");
   svgAllemagne = svgDoc.getElementById("allemagne");
   svgLuxembourg = svgDoc.getElementById("luxembourg");
@@ -51,12 +41,6 @@ svgExt.addEventListener("load",function() {
   
   svgLuxembourg.addEventListener("mouseenter", svgCountryInfo);
   svgLuxembourg.addEventListener("mouseleave", svgCountryHide);
-   
-=======
-
-  // On va rendre "sensible" un élément du SVG
-  svgEspagne.addEventListener("mouseover", svgCountryInfo);
->>>>>>> origin/master
 
  }, false);
 
@@ -66,6 +50,7 @@ function svgCountryInfo(){
   // on obtient l'ID de l'élément survolé:
 
   var CountryID = this.id;
+  console.log("svgCountryInfo: "+CountryID);
 
   // on en déduit l'élément à rendre visible
   var CountryInfoClass = "." + CountryID + "hover";
@@ -73,7 +58,6 @@ function svgCountryInfo(){
 
   CountryInfo.style.display="block";
 
-<<<<<<< HEAD
 }
 
 function svgCountryHide(){
@@ -82,6 +66,7 @@ function svgCountryHide(){
   // on obtient l'ID de l'élément survolé:
   
   var CountryID = this.id;
+  console.log("svgCountryHide: "+CountryID);
 
   // on en déduit l'élément à rendre visible
   var CountryInfoClass = "." + CountryID + "hover";
@@ -91,6 +76,3 @@ function svgCountryHide(){
 
 }
 
-=======
-}
->>>>>>> origin/master
