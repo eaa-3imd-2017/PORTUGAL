45d799e10e955c9ead09b15d1541d34c3109a2bd
Alice Christen <achristen07@gmail.com>
2017-05-31 10:28:10 +0200

    maj 31.5

--- js/pagetransition.js
+++ js/pagetransition.js
@@ -1,4 +1,29 @@
 // JavaScript Document
+//Mobile
+
+$( "#menu-mobile a" ).click(function() {
+ 
+  // récupérer l'élément cliqué
+   var cible = $(this).attr('href');
+  
+    // 1. masquer le niveau actif
+    $( ".niveauactif" ).hide( 2000, function() {
+  
+  // retirer la classe de l'élèment caché
+  $(this).removeClass( "niveauactif" );
+  
+  // afficher le niveau correspondant à cet attribu
+    // 2. afficher le nouveau niveau
+	
+  $(cible).fadeIn( 3000);
+  $(cible).addClass( "niveauactif" );
+  });
+
+  
+  return false;
+});
+
+//Desktop
 
 $( "#menu-desktop a" ).click(function() {
  
@@ -22,25 +47,3 @@ $( "#menu-desktop a" ).click(function() {
   return false;
 });
 
-
-$( "#menu-mobile a" ).click(function() {
- 
-  // récupérer l'élément cliqué
-   var cible = $(this).attr('href');
-  
-    // 1. masquer le niveau actif
-    $( ".niveauactif" ).hide( 2000, function() {
-  
-  // retirer la classe de l'élèment caché
-  $(this).removeClass( "niveauactif" );
-  
-  // afficher le niveau correspondant à cet attribu
-    // 2. afficher le nouveau niveau
-	
-  $(cible).fadeIn( 3000);
-  $(cible).addClass( "niveauactif" );
-  });
-
-  
-  return false;
-});
--- js/svg-animate.js
+++ js/svg-animate.js
@@ -1,30 +1,19 @@
-<<<<<<< HEAD
 // JavaScript Document
-=======
->>>>>>> origin/master
+
 /*
  * SVG Animate
  */
 
 // Le code suivant charge le fichier SVG externe, 
 // afin de le rendre accessible par Javascript
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
 var svgExt = document.getElementById("carteeurope");
-var svgDoc;
-
-console.log(svgExt);
 
 // On définit tous les objets SVG qu'on souhaite ensuite manipuler
-var svgEspagne;
-<<<<<<< HEAD
-var svgUk;
-var svgAllemagne;
-var svgLuxembourg;
-=======
->>>>>>> origin/master
+var svgDoc, svgEspagne, svgUk, svgAllemagne, svgLuxembourg;
+
+// MediaQuery Desktop
+var mq = window.matchMedia('(min-width: 700px)');
 
 svgExt.addEventListener("load",function() {
 
@@ -34,7 +23,6 @@ svgExt.addEventListener("load",function() {
   // Le SVG est chargé, on peut maintenant
   // définir les formes qu'on souhaite manipuler
   svgEspagne = svgDoc.getElementById("espagne");
-<<<<<<< HEAD
   svgUk = svgDoc.getElementById("uk");
   svgAllemagne = svgDoc.getElementById("allemagne");
   svgLuxembourg = svgDoc.getElementById("luxembourg");
@@ -51,12 +39,6 @@ svgExt.addEventListener("load",function() {
   
   svgLuxembourg.addEventListener("mouseenter", svgCountryInfo);
   svgLuxembourg.addEventListener("mouseleave", svgCountryHide);
-   
-=======
-
-  // On va rendre "sensible" un élément du SVG
-  svgEspagne.addEventListener("mouseover", svgCountryInfo);
->>>>>>> origin/master
 
  }, false);
 
@@ -65,7 +47,10 @@ function svgCountryInfo(){
   // ce qui se passe au hover:
   // on obtient l'ID de l'élément survolé:
 
+ if(mq.matches) {
+
   var CountryID = this.id;
+  console.log("svgCountryInfo: "+CountryID);
 
   // on en déduit l'élément à rendre visible
   var CountryInfoClass = "." + CountryID + "hover";
@@ -73,7 +58,8 @@ function svgCountryInfo(){
 
   CountryInfo.style.display="block";
 
-<<<<<<< HEAD
+  }
+
 }
 
 function svgCountryHide(){
@@ -81,7 +67,10 @@ function svgCountryHide(){
   // ce qui se passe au hover:
   // on obtient l'ID de l'élément survolé:
   
+ if(mq.matches) {
+
   var CountryID = this.id;
+  console.log("svgCountryHide: "+CountryID);
 
   // on en déduit l'élément à rendre visible
   var CountryInfoClass = "." + CountryID + "hover";
@@ -89,8 +78,6 @@ function svgCountryHide(){
 
   CountryInfo.style.display="none";
 
-}
+  }
 
-=======
 }
->>>>>>> origin/master
